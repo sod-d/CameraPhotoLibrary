730877bb8611ab5c2b9ed4563ffeb35dcd968b37
sodud <sodudwns102@gmail.com>
2024-01-16 09:56:48 +0900

    Initial Commit

---
+++ CameraPhotoLibrary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraPhotoLibrary
+//
+//  Created by 유라클 on 2024/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CameraPhotoLibraryUITests/CameraPhotoLibraryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CameraPhotoLibraryUITestsLaunchTests.swift
+//  CameraPhotoLibraryUITests
+//
+//  Created by 유라클 on 2024/01/16.
+//
+
+import XCTest
+
+final class CameraPhotoLibraryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
